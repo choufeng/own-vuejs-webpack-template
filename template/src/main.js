ad6f51a62d1064601127ed1b15b4870f44bf0549
jiaxia <xiajia@vip.qq.com>
2016-09-27 18:10:24 +0800

    init

--- template/src/main.js
+++ template/src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+let router = new VueRouter()
+
+router.map({
+  '/': {
+    name: 'index',
+    component: function (resolve) {
+      require(['./components/Index'], resolve)
+    }
+  }
+})
+
+router.beforeEach(function () {
+  window.scrollTo(0, 0)
+})
+
+router.redirect({
+  '*': '/'
+})
+
+router.start(App, 'app')
